e67045db485c0ecf7018bcf011d35775607200e8
NadjahroS <NadjahroS@outlook.com>
2025-06-25 22:04:08 +0200

    Updating a file now works by reading it before writing and starting from the last page of comments

--- src/main/java/com/xo/Main.java
+++ src/main/java/com/xo/Main.java
@@ -17,16 +17,17 @@ public class Main {
     public static void main(String[] args) {
         new GUI();
 
+//        steamScraper.getCoordinates("https://steamcommunity.com/app/386180/eventcomments/7264963628761167032", 25);
 //        steamScraper.getHtml("https://steamcommunity.com/app/386180/eventcomments/598528021200225035_");
 //        steamScraper.getHtml("https://steamcommunity.com/app/386180/eventcomments/599642060902915490");
 //        redditScraper.getHtml("https://www.reddit.com/r/Crossout/comments/1ivgcvy/find_a_treasure/");
 
-        try {
-            gridGenerator.editMetadata("src/output/steam-15-11-2024.jpeg", "test.com", 10);
+//        try {
+//            gridGenerator.editMetadata("src/output/steam-15-11-2024.jpeg", "test.com", 10);
 //            gridGenerator.paintGrid();
 //            gridGenerator.fillGrid("steam-15-11-2024.txt");
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 }
--- src/main/java/com/xo/scrapers/SteamScraper.java
+++ src/main/java/com/xo/scrapers/SteamScraper.java
@@ -7,10 +7,9 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 import javax.swing.*;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
@@ -31,6 +30,10 @@ public class SteamScraper {
     public SteamScraper() {
     }
 
+    public void getCoordinates(String url) {
+        getCoordinates(url, 1);
+    }
+
     /**
      * Web-scraper for comments from the Steam Crossout find the treasure contests.
      * After defining the starting variables it takes the HTML from the url.
@@ -38,12 +41,13 @@ public class SteamScraper {
      * Each loop it takes the coordinates from the subsequent pages.
      * Finally, it sorts the coordinates, writes them to a file, from which a jpeg is made and metadata gets added to.
      * @param url The Steam url to be scraped
+     * @param start The starting point (1 if a new url or another number if you want to update)
      */
-    public void getCoordinates(String url) {
+    public void getCoordinates(String url, int start) {
         org.jsoup.nodes.Document doc = null;
         BufferedWriter writer = null;
 
-        int page = 1;
+        int page = start;
         String baseUrl = url;
 
         // Using set instead of array for automatic duplicate removal
@@ -51,9 +55,12 @@ public class SteamScraper {
 
         try {
             while (true) {
-                progressText.append("Checking page: " + page);
-                progressText.append("\n");
+                if (progressText != null) {
+                    progressText.append("Checking page: " + page);
+                    progressText.append("\n");
+                }
                 System.out.println("Checking page: " + page);
+
                 // Reset url and append next comment page
                 if (page > 1) {
                     url = baseUrl;
@@ -64,7 +71,7 @@ public class SteamScraper {
 //              System.out.println(doc);
 
                 // Takes a date from the html, then the title, takes the useful part and converts it to LocalDate format.
-                if (page == 1) {
+                if (page == start) {
                     Element dateSpan = doc.selectFirst(".date");
                     if (dateSpan == null) {
                         progressText.append("Date not found");
@@ -75,17 +82,31 @@ public class SteamScraper {
                     String date = dateTime.substring(0, dateIndex);
                     DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d MMMM, yyyy", Locale.ENGLISH);
                     String localDate = LocalDate.parse(date.trim(), formatter).format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
+                    // System.out.println(localDate);
 
                     filename = "steam-" + localDate;
                     File outputFile = new File("src/output/", ("steam-" + localDate + ".txt"));
+
+                    // Checks if the file already exists before wiping it with the writer (mainly for updating)
+                    if (Files.exists(Path.of("src/output/" + filename + ".txt"))) {
+                        System.out.println("Found file: " + filename + ".txt");
+                        BufferedReader reader = new BufferedReader(new FileReader(outputFile));
+                        String data;
+                        while ((data = reader.readLine()) != null ) {
+                            coordinates.add(data);
+                            System.out.println(data);
+                        }
+                        reader.close();
+                    }
                     writer = new BufferedWriter(new FileWriter(outputFile));
-                    // System.out.println(localDate);
                 }
 
                 // Takes all comments from the html, stops the program if there are no comments left
                 Elements comments = doc.select(".commentthread_comment_text");
                 if (comments.isEmpty()) {
-                    progressText.append("Page:" + page + "had no more comments");
+                    if (progressText != null) {
+                        progressText.append("Page:" + page + "had no more comments");
+                    }
                     System.out.println("Page:" + page + "had no more comments");
                     break;
                 }
@@ -105,9 +126,7 @@ public class SteamScraper {
                 page++;
                 Thread.sleep(4000);
             }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
+
             List<String> sortedCoordinates = new ArrayList<>(coordinates);
             sortedCoordinates.sort((a, b) -> {
                 int letterCompare = Character.compare(a.charAt(0), b.charAt(0));
@@ -118,89 +137,45 @@ public class SteamScraper {
                 return Integer.compare(aNum, bNum);
             });
 
-            try {
-                if (writer != null) {
-                    for (String coord : sortedCoordinates) {
-                        writer.write(coord);
-                        writer.newLine();
-                    }
-                    writer.close();
-                }
-
-                gridGenerator.fillGrid(filename + ".txt");
-                gridGenerator.editMetadata("src/output/" + filename + ".jpeg", baseUrl, page-1);
-            } catch (IOException e) {
-                e.printStackTrace();
+            for (String coord : sortedCoordinates) {
+                writer.write(coord);
+                writer.newLine();
             }
-//          System.out.println(sortedCoordinates);
+            writer.close();
+
+            gridGenerator.fillGrid(filename + ".txt");
+            gridGenerator.editMetadata("src/output/" + filename + ".jpeg", baseUrl, page-1);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
+//        } finally {
+//            List<String> sortedCoordinates = new ArrayList<>(coordinates);
+//            sortedCoordinates.sort((a, b) -> {
+//                int letterCompare = Character.compare(a.charAt(0), b.charAt(0));
+//                if (letterCompare != 0) return letterCompare;
+//                // compare number part
+//                int aNum = Integer.parseInt(a.substring(1));
+//                int bNum = Integer.parseInt(b.substring(1));
+//                return Integer.compare(aNum, bNum);
+//            });
+//
+//            try {
+//                if (writer != null) {
+//                    for (String coord : sortedCoordinates) {
+//                        writer.write(coord);
+//                        writer.newLine();
+//                    }
+//                    writer.close();
+//                }
+//
+//                gridGenerator.fillGrid(filename + ".txt");
+//                gridGenerator.editMetadata("src/output/" + filename + ".jpeg", baseUrl, page-1);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+////          System.out.println(sortedCoordinates);
+//        }
         // System.out.println(doc);
-    }
-
-    public void updateCoordinates(String url, int start, String file) {
-        org.jsoup.nodes.Document doc = null;
-        BufferedWriter writer = null;
-
-        int page = start;
-        String baseUrl = url;
-
-        // Using set instead of array for automatic duplicate removal
-        Set<String> coordinates = new HashSet<>();
-
-        try {
-            while (true) {
-                progressText.append("Checking page: " + page);
-                progressText.append("\n");
-                System.out.println("Checking page: " + page);
-                // Reset url and append next comment page
-                if (page > 1) {
-                    url = baseUrl;
-                    url += ("?ctp=" + page);
-                }
-
-                doc = Jsoup.connect(url).userAgent("Mozilla").data("name", "jsoup").get();
-//              System.out.println(doc);
-
-                // Takes all comments from the html, stops the program if there are no comments left
-                Elements comments = doc.select(".commentthread_comment_text");
-                if (comments.isEmpty()) {
-                    progressText.append("Page:" + page + "had no more comments");
-                    System.out.println("Page:" + page + "had no more comments");
-                    break;
-                }
-                coordinates.addAll(utils.getCoordinates(comments));
-//                System.out.println(coordinates);
-                page++;
-                Thread.sleep(4000);
-            }
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        } finally {
-            List<String> sortedCoordinates = new ArrayList<>(coordinates);
-            sortedCoordinates.sort((a, b) -> {
-                int letterCompare = Character.compare(a.charAt(0), b.charAt(0));
-                if (letterCompare != 0) return letterCompare;
-                // compare number part
-                int aNum = Integer.parseInt(a.substring(1));
-                int bNum = Integer.parseInt(b.substring(1));
-                return Integer.compare(aNum, bNum);
-            });
-
-            try {
-                if (writer != null) {
-                    for (String coord : sortedCoordinates) {
-                        writer.write(coord);
-                        writer.newLine();
-                    }
-                    writer.close();
-                }
-
-                gridGenerator.fillGrid(filename + ".txt");
-                gridGenerator.editMetadata("src/output/" + filename + ".jpeg", baseUrl, page-1);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
     }
 
     public String getFilename() {
